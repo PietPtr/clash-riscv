30930556404a7a7f78abf45a757d48eb42da53e8
Pieter Staal <pieterstaal@outlook.com>
2020-02-25 00:11:33 +0100

    Add gitignore

--- rv32i/Decode.hs
+++ rv32i/Decode.hs
@@ -11,26 +11,26 @@ constructImmediate :: InstructionForm -> Immediate
 constructImmediate (ITypeForm imm11'0 _ _ _ _) = sign .|. rest
     where
         sign :: Signed 32 = if (testBit imm11'0 11) then 0xfffff800 else 0
-        rest :: Signed 32 = fromInteger $ toInteger imm11'0 .&. 0x7ff
+        rest :: Signed 32 = conv imm11'0 .&. 0x7ff
 constructImmediate (STypeForm imm11'5 _ _ _ imm4'0 _) = sign .|. rest
     where
         sign :: Signed 32 = if (testBit imm11'5 6) then 0xfffff800 else 0
-        rest :: Signed 32 = fromInteger $ ((toInteger imm11'5) `shift` 5) .|. (toInteger imm4'0)
+        rest :: Signed 32 = conv $ ((conv imm11'5 :: Unsigned 12) `shift` 5) .|. (conv imm4'0 :: Unsigned 12)
 constructImmediate (BTypeForm imm12 imm10'5 _ _ _ imm4'1 imm11 _) = sign .|. rest
     where
         sign :: Signed 32 = if (imm12 == 1) then 0xfffff000 else 0
-        rest :: Signed 32 = fromInteger $ (pos11 .|. (pos10'5 .|. pos4'1))
-        pos11 = toInteger imm11 `shift` 11
-        pos10'5 = toInteger imm10'5 `shift` 5
-        pos4'1 = toInteger imm4'1 `shift` 1
-constructImmediate (UTypeForm imm31'12 _ _) = fromInteger $ toInteger imm31'12 `shift` 12
+        rest :: Signed 32 = conv (pos11 .|. (pos10'5 .|. pos4'1))
+        pos11 :: Unsigned 32 = conv imm11 `shift` 11
+        pos10'5 :: Unsigned 32 = conv imm10'5 `shift` 5
+        pos4'1 :: Unsigned 32 = conv imm4'1 `shift` 1
+constructImmediate (UTypeForm imm31'12 _ _) = conv imm31'12 `shift` 12
 constructImmediate (JTypeForm imm20 imm10'1 imm11 imm19'12 _ _) = sign .|. rest
     where
         sign :: Signed 32 = if (imm20 == 1) then 0xfff00000 else 0
-        rest :: Signed 32 = fromInteger $ (pos19'12 .|. (pos11 .|. (pos10'1)))
-        pos19'12 = toInteger imm19'12 `shift` 12
-        pos11 = toInteger imm11 `shift` 11
-        pos10'1 = toInteger imm10'1 `shift` 1
+        rest :: Signed 32 = conv (pos19'12 .|. (pos11 .|. (pos10'1)))
+        pos19'12 :: Unsigned 32 = conv imm19'12 `shift` 12
+        pos11 :: Unsigned 32 = conv imm11 `shift` 11
+        pos10'1 :: Unsigned 32 = conv imm10'1 `shift` 1
 
 
 decode :: InstructionForm -> Instruction
