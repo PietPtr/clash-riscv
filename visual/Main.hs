0b8a39a74e7d7ab002bb38855d9da0311ce43406
Pieter Staal <pieterstaal@outlook.com>
2020-04-04 23:59:43 +0200

    Add PC and regs

--- visual/Main.hs
+++ visual/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Clash.Prelude hiding (Text, map, (++), length, zip, take)
 import Core hiding (Tick)
-import BaseTypes
+import BaseTypes hiding (trace)
 import Debug
 
 import Eventloop.Core
@@ -31,12 +31,16 @@ canvasId = 1
 
 white   = (235, 228, 211, 255)
 black   = ( 99,  97,  90, 255)
+nothing = (  0,   0,   0,   0)
+red     = (255,   0,   0, 255)
 
 canvasSize = (1240, 1024)
 
 drawSF name shape = [ Draw $ Stateful name 0 shape ]
 
 txtShape (x,y) str = Text str "Courier" 14 (Point (x,y)) AlignLeft black 1 black Nothing
+txtShapeR (x,y) str = (txtShape (x, y) str) { alignment = AlignRight }
+
 
 defaultCanvasSetup = C.SetupCanvas canvasId 1 canvasSize (C.CSSPosition C.CSSFromCenter (C.CSSPercentage 50, C.CSSPercentage 50))
 
@@ -80,9 +84,8 @@ eventloop state event = case event of
         ])
 
     InTimer (Tick "autorun") -> (state',
-        map (\a -> OutStatefulGraphics 1 [a]) graphics)
+        renderCore (coreState'))
         where
-            graphics = renderLines (10, 10) $ formatMemory $ memory $ coreState state
             state' = state { coreState = coreState' }
             coreState' = core (coreState state) 0
 
@@ -94,11 +97,75 @@ eventloop state event = case event of
 
     _ -> (state, [])
 
-renderLines :: (X, Y) -> [String] -> [StatefulGraphicsOut]
-renderLines _ [] = []
-renderLines (x, y) (line:lines) = (drawSF ("name" ++ (show y)) shape) ++ renderLines (x, y + 20) lines
-    where shape = txtShape (x, y) line
+onCanvas :: [StatefulGraphicsOut] -> [Out]
+onCanvas graphics = map (\a -> OutStatefulGraphics 1 [a]) graphics
 
+
+renderCore :: SystemState -> [Out]
+renderCore state = renderedMemory ++ renderedRegisters ++ renderedPC
+    where
+        renderedMemory    = renderMemory $ memory $ state
+        renderedRegisters = renderRegisters $ registers $ state
+        renderedPC        = renderPC $ pc $ state
+
+renderRegisters :: RegisterBank -> [Out]
+renderRegisters regs = onCanvas $ renderLines (700, 31) $ formatRegisters $ regs
+
+renderPC :: PC -> [Out] -- TODO: maak alsjeblieft van deze magic numbers constanten...
+renderPC pc = numeric ++ highlighter
+    where
+        numeric         = onCanvas $ drawSF "pc" --klopt echt totaal niet lol
+            $ txtShape (700, 691) ("      PC" +-+ showHex (conv (pc * 4)) 8)
+        highlighter     = onCanvas $ drawSF "pc-highlight" shape
+        shape           = Rectangle pos (70, 18) nothing 1 red Nothing
+        pos             = Point (34 + 75.7 * xmod, 25 + 20 * ymod)
+        xmod :: Float   = conv (pc `mod` 8)
+        ymod :: Float   = (fromIntegral . floor) $ (fromIntegral pc) / 8
+
+formatRegisters :: RegisterBank -> [String]
+formatRegisters regs = formatted
+    where
+        formatted = mapi (\r i -> (regnames i) +-+ "x" ++ (show i) +-+ (showHex r 8) ) listRegs'
+        listRegs' :: [Integer] = map conv listRegs
+        listRegs :: [Unsigned 32] = map conv (toList regs)
+
+regnames :: Integer -> String
+regnames reg = case reg of
+    0  -> "zero"
+    1  -> "ra  "
+    2  -> "sp  "
+    3  -> "gp  "
+    4  -> "tp  "
+    5  -> "t0  "
+    6  -> "t1  "
+    7  -> "t2  "
+    8  -> "fp  "
+    9  -> "s1  "
+    10 -> "a0  "
+    11 -> "a1  "
+    12 -> "a2  "
+    13 -> "a3  "
+    14 -> "a4  "
+    15 -> "a5  "
+    16 -> "a6  "
+    17 -> "a7  "
+    18 -> "s2  "
+    19 -> "s3  "
+    20 -> "s4  "
+    21 -> "s5  "
+    22 -> "s6  "
+    23 -> "s7  "
+    24 -> "s8  "
+    25 -> "s9  "
+    26 -> "s10 "
+    27 -> "s11 "
+    28 -> "t3  "
+    29 -> "t4  "
+    30 -> "t5  "
+    31 -> "t6  "
+
+renderMemory :: Memory -> [Out]
+renderMemory memory = onCanvas $ renderLines (10, 10) $ formatMemory $ memory
 
 formatMemoryList :: [Integer] -> [String]
 formatMemoryList mem = header : (mapi (\chunk i -> formatLine chunk i) chunks)
@@ -106,11 +173,8 @@ formatMemoryList mem = header : (mapi (\chunk i -> formatLine chunk i) chunks)
         header = "         " ++ (intercalate "       " (map (\a -> showHex a 2) [0,4..0x1c]))
         chunks = chunksOf 8 mem
         formatLine words index =
-            (take 2 (showHex (index * 32) 3)) ++ " " ++
+            (take 2 (showHex (index * 32) 3)) +-+
             (intercalate " " $ map (\a -> printf "%08x" (a)) words)
-
-showHex :: Integer -> Integer -> String
-showHex n d = printf ("%0" ++ (show d) ++ "x") n
 
 formatMemory :: Memory -> [String]
 formatMemory mem = formatMemoryList listInts
@@ -118,6 +182,18 @@ formatMemory mem = formatMemoryList listInts
         listInts     :: [Integer]     = map (conv) listUnsigned
         listUnsigned :: [Unsigned 32] = map (conv) (toList mem)
 
+
+showHex :: Integer -> Integer -> String
+showHex n d = printf ("%0" ++ (show d) ++ "x") n
+
+renderLines :: (X, Y) -> [String] -> [StatefulGraphicsOut]
+renderLines _ [] = []
+renderLines (x, y) (line:lines) = (drawSF ("name" ++ (show y)) shape) ++ renderLines (x, y + 20) lines
+    where shape = txtShape (x, y) line
+
+
 mapi :: (a -> Integer -> b) -> [a] -> [b]
 mapi f as = map (\(a, i) -> f a i) combined
     where combined = zip as [0..]
+
+a +-+ b = a ++ " " ++ b
